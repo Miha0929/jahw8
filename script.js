289f28be629afb6555a9898b0b5a148b64f6eca5
Miha0929 <mishasobolev004@gmail.com>
2026-02-24 22:57:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("hi");
+
+const user = {
+  name: "John",
+  age: 30,
+  email: "john@example.com",
+  isAdmin: true
+};
+
+console.log(user.name);
+
+// задание 2
+
+user.course = 'JavaScript';
+console.log(user.course);
+
+let product = {
+  title: "Laptop",
+  price: 999.99,
+  inStock: true
+};
+ if (product.inStock === true) {
+    console.log("Product is available")};
+        
